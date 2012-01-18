11e435c9fc21efeee2186e69a226ad4fe426ccf1
jeffrey kaditz <jeff.kaditz@gmail.com>
2012-01-17 16:19:21 -0800

    more renaming.

--- test/stormjoin/test/core.clj
+++ test/stormjoin/test/core.clj
@@ -1,8 +1,8 @@
-(ns pjoin.test.core
-  (:use [pjoin.core])
+(ns stormjoin.test.core
+  (:use [stormjoin.core])
   (:use [clojure.test])
   (:use [loom.graph]))
 
 (deftest replace-me ;; FIXME: write
-  (is (= (loom.graph/digraph ["A" "SplitBolt-id3"] ["SplitBolt-id3" "B"] ["SplitBolt-id3" "C"]) (pjoin.bolts/createSplitBolt "id3" #(> 3 %) "A" ["B" "C"]))))
+  (is (= (loom.graph/digraph ["A" "SplitBolt-id3"] ["SplitBolt-id3" "B"] ["SplitBolt-id3" "C"]) (stormjoin.bolts/createSplitBolt "id3" #(> 3 %) "A" ["B" "C"]))))
   ;(is false "No tests have been written."))
