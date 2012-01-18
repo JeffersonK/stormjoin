fbd2ca45c20c695bdb897cb5754cc0af00c96e5c
jeffrey kaditz <jeff.kaditz@gmail.com>
2012-01-17 16:26:02 -0800

    added autodoc and autotest to project

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject stormjoin "0.1-SNAPSHOT"
-  :description "parallel join planner"
+  :description "storm query compiler and planner"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.satta/loom "0.1.0-SNAPSHOT"]]
   :uberjar-exclusions [#"META-INF.*"]
@@ -8,9 +8,12 @@
                      [swank-clojure "1.2.1"]
                      [lein-ring "0.4.5"]
 		     [lein-autodoc "0.9.0"]
-                     ]
+                     [com.stuartsierra/lazytest "1.1.2"]
+                     [lein-autotest "1.1.0"]]
+  :repositories {"stuartsierra-releases" "http://stuartsierra.com/maven2"}
   :jvm-opts ["-Djava.library.path=/usr/local/lib:/opt/local/lib:/usr/lib"]
   :extra-classpath-dirs [""]
+  :autodoc { :name "Storm Compiler", :page-title "Storm Compiler API Documentation"}
   :aot :all
   :main stormjoin.core
   )
