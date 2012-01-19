4c15b7a7a916c21680f7cb740ae8c5b45205a6f3
Jeff Kaditz <jeff.kaditz@gmail.com>
2012-01-18 18:02:19 -0800

    cleaned up input format to the main joni planner function.

--- src/stormjoin/core.clj
+++ src/stormjoin/core.clj
@@ -14,10 +14,10 @@
 (defn- processStreamList  
   ([Streams] (processStreamList Streams 0))
   ([Streams cnt] (when-not (= 0 (count Streams))
-                   (let [ [streamId paralellism] (first Streams)
+                   (let [ [streamId joinParalellism unionParallelism] (first Streams)
                           streamParts (if (= 0 cnt)
-                                        (vec (map #(str streamId % ) (range paralellism)))
-                                        (vec (map #(str "PJOIN(" streamId % ")") (range paralellism))))
+                                        (vec (map #(str streamId % ) (range joinParalellism)))
+                                        (vec (map #(str "PJOIN(" streamId % ")") (range joinParalellism))))
                                         ;(println "Got: " streamId ", " paralellism))
                                         ;(println "Parts: " streamParts)
                                         ]
@@ -38,31 +38,35 @@
 ;;TODO: if anchor stream has parallelism > 1 need to do union first before dup
 ;;QQQQ: is it better to replicate the union bolts in the intermediate stages or have a single one and the duplicate. the latter is less parallel but less resource intensive also
 ;;TODO: need a setting on whether we are optimizing for slot usage (minimize) or for speed of computation potentially much higher bandwidth do to data being replicated.
-(defn- breadthFirstJoinBuilder [predicate thisStageNo finalStageNo stageInputs stagePartitions unionStageParallelism]  
-  (cond
-   (= 0 thisStageNo) (let [upstream-sub-graph (breadthFirstJoinBuilder predicate (+ 1 thisStageNo) finalStageNo (first stagePartitions) (rest stagePartitions) (rest unionStageParallelism))
-                           anchorStream (first stageInputs)
-                           anchorPartCount (count (first stagePartitions))
-                           anchorUnionBolt (if (< 1 anchorPartCount)
-                                             (stormjoin.bolts/createUnionBolt anchorStream (first stagePartitions) (str "DupBolt-" anchorStream))
-                                             nil)
-                           dupBolt (if (< 1 anchorPartCount)
-                                     (stormjoin.bolts/createDupBolt (str anchorStream) (str "UnionBolt-" anchorStream) (second stagePartitions))
-                                     (stormjoin.bolts/createDupBolt (str anchorStream) anchorStream (second stagePartitions)))
-                           splitBolts (map (fn [x y] (stormjoin.bolts/createSplitBolt (str x) (str predicate) y x)) (rest stagePartitions) (rest stageInputs))
-                           sub-graph (apply (partial loom.graph/digraph dupBolt) splitBolts)]
-                       (apply loom.graph/digraph (filter #(not (nil? %)) [sub-graph upstream-sub-graph anchorUnionBolt])))
-   (< thisStageNo finalStageNo) (let [upstream-sub-graph (breadthFirstJoinBuilder predicate (+ 1 thisStageNo) finalStageNo (first stagePartitions) (rest stagePartitions) (rest unionStageParallelism))
-                                      ;;TODO: abilty to specify parallelism of intermediate unions
-                                      ;;unionBoltCount (min (count (second stagePartitions)) (if (< 0 (count unionStageParallelism))
-                                      ;;                                                       (first unionStageParallelism)
-                                      ;;                                                       (count (second stagePartitions))))                                      
-                                      ;;unionPartitions (partition-into (second stagePartitions) unionBoltCount)
-                                      unionBolts (map (fn [x] (stormjoin.bolts/createUnionBolt (str x) (first stagePartitions) x)) (second stagePartitions))]
-                                  ;;(println unionBoltCount unionPartitions (second stagePartitions))
-                                  (reduce #(loom.graph/digraph %1 %2) upstream-sub-graph unionBolts))
-   (= thisStageNo finalStageNo) (stormjoin.bolts/createUnionBolt (str stagePartitions) (first stagePartitions) "*END*")
-   :else (println "WTF?!!!"));;TODO: throw an error shouldn't get here
+(defn- breadthFirstJoinBuilder
+  ([predicate streams] (let [stream-parts (processStreamList streams)]
+                         (breadthFirstJoinBuilder predicate 0 (- (count streams) 1) (map #(first %) streams) stream-parts (map #(last %) streams))))
+  ([predicate thisStageNo finalStageNo stageInputs stagePartitions unionStageParallelism]  
+     (cond
+      (= 0 thisStageNo) (let [upstream-sub-graph (breadthFirstJoinBuilder predicate (+ 1 thisStageNo) finalStageNo (first stagePartitions) (rest stagePartitions) (rest unionStageParallelism))
+                              anchorStream (first stageInputs)
+                              anchorPartCount (count (first stagePartitions))
+                              anchorUnionBolt (if (< 1 anchorPartCount)
+                                                (stormjoin.bolts/createUnionBolt anchorStream (first stagePartitions) (str "DupBolt-" anchorStream))
+                                                nil)
+                              dupBolt (if (< 1 anchorPartCount)
+                                        (stormjoin.bolts/createDupBolt (str anchorStream) (str "UnionBolt-" anchorStream) (second stagePartitions))
+                                        (stormjoin.bolts/createDupBolt (str anchorStream) anchorStream (second stagePartitions)))
+                              splitBolts (map (fn [x y] (stormjoin.bolts/createSplitBolt (str x) (str predicate) y x)) (rest stagePartitions) (rest stageInputs))
+                              sub-graph (apply (partial loom.graph/digraph dupBolt) splitBolts)]
+                          (apply loom.graph/digraph (filter #(not (nil? %)) [sub-graph upstream-sub-graph anchorUnionBolt])))
+      (< thisStageNo finalStageNo) (let [upstream-sub-graph (breadthFirstJoinBuilder predicate (+ 1 thisStageNo) finalStageNo (first stagePartitions) (rest stagePartitions) (rest unionStageParallelism))
+                                         ;;TODO: abilty to specify parallelism of intermediate unions
+                                         ;;unionBoltCount (min (count (second stagePartitions)) (if (< 0 (count unionStageParallelism))
+                                         ;;                                                       (first unionStageParallelism)
+                                         ;;                                                       (count (second stagePartitions))))                                      
+                                         ;;unionPartitions (partition-into (second stagePartitions) unionBoltCount)
+                                         unionBolts (map (fn [x] (stormjoin.bolts/createUnionBolt (str x) (first stagePartitions) x)) (second stagePartitions))]
+                                     ;;(println unionBoltCount unionPartitions (second stagePartitions))
+                                     (reduce #(loom.graph/digraph %1 %2) upstream-sub-graph unionBolts))
+      (= thisStageNo finalStageNo) (stormjoin.bolts/createUnionBolt (str stagePartitions) (first stagePartitions) "*END*")
+      :else (println "WTF?!!!"));;TODO: throw an error shouldn't get here
+     )
   )
 
 ;;
@@ -79,10 +83,11 @@
 
 (defn -main [& args]
   ;;(loom.io/view (breadthFirstJoinBuilder "f(x,y)" 0 3 ["A" "B" "C" "D"] [["A0" "A1"] ["BO" "B1"] ["C0" "C1" "C2" "C3"] ["D0" "D1"]] []));2 3 2]))
-  (def stream-inputs [["A" 2] ["B" 2] ["C" 4]])
+  (def stream-inputs [["A" 2 2] ["B" 2 2] ["C" 4 4]])
   (def stream-parts (processStreamList stream-inputs))
-  ;(println stream-parts)
-  (loom.io/view (breadthFirstJoinBuilder "f(x,y)" 0 (- (count stream-inputs) 1) ["A" "B" "C"] stream-parts [2 2 4]))
+  (println stream-parts)
+  (loom.io/view (breadthFirstJoinBuilder "f(x,y)" stream-inputs))
+  ;;(loom.io/view (breadthFirstJoinBuilder "f(x,y)" 0 (- (count stream-inputs) 1) ["A" "B" "C"] stream-parts [2 2 4]))
   ;(loom.io/view (breadthFirstJoinBuilder "f(x,y)" 0 4 ["A" "B" "C" "D" "E"] [["A0" "A1"] ["BO" "B1"] ["C0" "C1" "C2" "C3"] ["D0" "D1"] ["E0" "E1" "E2" "E3"]] [3 2 4]))
   ;(loom.io/view (breadthFirstJoinBuilder "f(x,y)" 0 3 ["A" "B" "C" "D"] [["A"] ["BO" "B1"] ["C0" "C1" "C2" "C3"] ["D0" "D1"]]))
   )
